07ea3013db399f8e9114a4a969e3b619db009692
guillaumeChretienRichardson <guillaume.c.richardson@gmail.com>
2025-03-18 09:31:16 -0400

    Pastilles centrées, le paths des chemins est render avant les territoires. Plus petite correction d'un des paths qui pointait vers l'eau.

--- assets/js/game/game.js
+++ assets/js/game/game.js
@@ -367,10 +367,10 @@ function updatePastillesPosition() {
     const territoireId = pastilles.item(i).getAttribute("territoire");
     const territoire = document.getElementById(territoireId);
     const bbox = territoire.getBoundingClientRect();
-
+    const offset = territoiresList[territoireId].pastille ?? {x:0.5, y:0.5};
     // Calculate the center of the bounding box
-    const centerX = bbox.x + bbox.width / 2;
-    const centerY = bbox.y + bbox.height / 2;
+    const centerX = bbox.x + bbox.width * (offset.x  || 0.5);
+    const centerY = bbox.y + bbox.height * (offset.y  || 0.5);
 
     pastilles.item(i).style.left = centerX - 12.5 + "px";
     pastilles.item(i).style.top = centerY - 12.5 + "px";
@@ -383,11 +383,12 @@ function createPastille(territoireId, playerId) {
 
   // Get the bounding box of the path
   const bbox = territoire.getBoundingClientRect();
-
+  
+  const offset = territoiresList[territoireId].pastille ?? {x:0.5, y:0.5};
   // Calculate the center of the bounding box
-  const centerX = bbox.x + bbox.width / 2;
-  const centerY = bbox.y + bbox.height / 2;
-
+  const centerX = bbox.x + bbox.width * (offset.x  || 0.5);
+  const centerY = bbox.y + bbox.height * (offset.y  || 0.5);
+  
   // Create a new div element
   const newDiv = document.createElement("div");
   newDiv.id = `pastille-${territoireId}`;
